30a8a6dbcb03220a5aff928f8fe159f5c93747c7
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-01 10:54:24 +0430

    Initial commit

---
+++ app/src/main/java/ir/ah/timerinjetpackcompose/MainActivity.kt
@@ -0,0 +1,32 @@
+package ir.ah.timerinjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ir.ah.timerinjetpackcompose.ui.theme.TimerInJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TimerInJetpackComposeTheme {
+
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TimerInJetpackComposeTheme {
+        
+    }
+}
